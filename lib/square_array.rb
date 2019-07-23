12f421110e8e92d89ad889f2055eda64f333cd0e
Megan Edmonds <meganr.edmonds@gmail.com>
2019-07-23 02:29:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  counter = 0 
+  while array[counter] do
+    new_array.push(array[counter]^2)
+    counter++
+  end
+  new_array
 end
